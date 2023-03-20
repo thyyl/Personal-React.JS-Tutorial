44ed0edd6c07f80bd953fa47a6952889f1a137bb
Kelvin <kelvincheah02@gmail.com>
2023-03-20 19:15:51 +0800

    test commit

--- Blog/src/Create.js
+++ Blog/src/Create.js
@@ -1,51 +1,67 @@
-import { useState } from 'react';
-import { useForm } from './useForm';
-import { useHistory } from 'react-router-dom';
+import { useState } from "react";
+import { useForm } from "./useForm";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
-    const [values, handleChange] = useForm({ title: "", body: "", author: "mario" });
-    const [isPending, setIsPending] = useState(false);
-    const history = useHistory();
+  // create
+  const [values, handleChange] = useForm({
+    title: "",
+    body: "",
+    author: "mario",
+  });
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
-    const handleSubmit = (e) => {
-        const title = values.title;
-        const body =  values.body;
-        const author = values.author;
-        
-        e.preventDefault();
-        const blog = { title, body, author };
+  const handleSubmit = (e) => {
+    const title = values.title;
+    const body = values.body;
+    const author = values.author;
 
-        setIsPending(true);
+    e.preventDefault();
+    const blog = { title, body, author };
 
-        fetch('http://localhost:8000/blogs/', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json"},
-            body: JSON.stringify(blog)
-        }).then(() => {
-            console.log("New blog added!")
-            setIsPending(false);
-            history.push('/');
-        })
-    }
+    setIsPending(true);
 
-    return (  
-        <div className="create">
-            <h2>Create a new blog!</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Blog Title: </label>
-                <input type="text" required name="title" value={values.title} onChange={handleChange}/>
-                <label>Blog Body: </label>
-                <textarea required name="body" value={values.body} onChange={handleChange}></textarea>
-                <label>Blog Author: </label>
-                <select name="author" value={values.author} onChange={handleChange}>
-                    <option value="mario">mario</option>
-                    <option value="yoshi">yoshi</option>
-                </select>
-                { !isPending && <button>Add Blog</button> }
-                { isPending && <button disabled>Blog Added</button> }
-            </form>
-        </div>
-    );
-}
- 
+    fetch("http://localhost:8000/blogs/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("New blog added!");
+      setIsPending(false);
+      history.push("/");
+    });
+  };
+
+  return (
+    <div className="create">
+      <h2>Create a new blog!</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog Title: </label>
+        <input
+          type="text"
+          required
+          name="title"
+          value={values.title}
+          onChange={handleChange}
+        />
+        <label>Blog Body: </label>
+        <textarea
+          required
+          name="body"
+          value={values.body}
+          onChange={handleChange}
+        ></textarea>
+        <label>Blog Author: </label>
+        <select name="author" value={values.author} onChange={handleChange}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Blog Added</button>}
+      </form>
+    </div>
+  );
+};
+
 export default Create;
